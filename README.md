09cf18097da356681e57223dcc987660240909b7
KarenLivia <karenliviaalves@gmail.com>
2022-08-19 19:47:02 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-dyrssn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-dyrssn)
---
+++ index.ts
@@ -0,0 +1,14 @@
+let title: string = 'MyApp';
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `
+<h1>${title}</h1>
+<hr/>
+<p>Teste parágrafo</p>
+`;
+
+console.log('Olá, mundo!');
+  for (let i = 1; i <= 10; i++){
+    if (i % 2 == 0){
+      console.log(i);
+    }
+  }
