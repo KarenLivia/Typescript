37caf0de5798d1af5d5042020ae6d5e4f0dea929
KarenLivia <karenliviaalves@gmail.com>
2022-08-19 21:56:17 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -9,3 +9,26 @@ let intervalId = setInterval(() => {
   counterDiv.innerHTML = counter.toString();
 },1000);
 
+
+for (let round = 1; round < 5; round ++){
+  if (round < 9){
+    console.log('Round '+round);
+    for (let i = 1; i < 21; i ++){
+      if (i % 2){
+        console.log('Tabata Activity');
+        for (let i = 1; i <= 21; i++){
+          if (i < 21){
+            console.log(i);
+          }
+        }
+      }
+      else{
+        console.log('Tabata Rest');
+        for (let i = 1; i <= 11; i++){
+          if (i < 11){
+            console.log(i);} 
+        } 
+      }
+    }
+  }
+}
